3697ca1c8f1ed57388884cf2949f9b0b008af2f7
Aashish <aashish.211607@ncit.edu.np>
2025-01-21 17:58:14 +0545

    views updated according to serializer

--- blog/urls.py
+++ blog/urls.py
@@ -1,23 +1,20 @@
 from django.urls import path
-
-from . import views
-from  .views import BlogList,createBlog,EditBlog,DeleteBlog,UserList,CommentView,CommentDetail,EditProfile,AdminCommentReviewView,approve_comment,delete_comment
+from .views import *
 
 urlpatterns = [
-     
-    
-    # path('add_blog/', views.submitBlog, name='add_blog'),
-    path('create_blog/', createBlog.as_view(), name='create_blog'),
-    path('edit_blog/<int:pk>',EditBlog.as_view() , name='edit_blog'),
-    path('delete_blog/<int:pk>',DeleteBlog.as_view() , name='delete_blog'),
-    path('user_dashboard/<int:pk>',UserList.as_view(),name='user_dashboard'),
-    path('like/<int:pk>',views.liked_blog, name='like'),
-    path('comments/<int:pk>',CommentView.as_view() , name='comments'),
-    path('<int:pk>/commentView/', CommentDetail.as_view(), name='commentDetail'),
-    path('update_profile/<int:pk>',EditProfile.as_view() , name='editProfile'),
-    path('admin/review-comments/', AdminCommentReviewView.as_view(), name='admin_review_comments'),
-    path('comment/<int:pk>/approve/', approve_comment, name='approve_comment'),
-    path('comment/<int:pk>/delete/', delete_comment, name='delete_comment'),
-  
+    path('blog_list/', blog_list, name='blog_list'),
+    path('blogs/<int:pk>/', blog_detail, name='blog_detail'),
+    path('blogs/<int:pk>/comments/', CommentAPIView.as_view(), name='create_comment'),
+    path('blogs/<int:pk>/like/', like_blog, name='like_blog'),
+    path('profile/update/', update_profile, name='update_profile'),
+    path('create/', CreateBlogAPIView.as_view(), name='create_blog'),
+    path('edit/<int:pk>/', EditBlogAPIView.as_view(), name='edit_blog'),
+    path('delete/<int:pk>/', DeleteBlogAPIView.as_view(), name='delete_blog'),
+    path('blogs/<int:pk>/translate/', TranslateBlogView.as_view(), name='translate_blog'),
+    path('approve_comment/<int:pk>/', ApproveCommentAPIView.as_view(), name='approve_comment'),
+    path('delete_comment/<int:pk>/', DeleteCommentAPIView.as_view(), name='delete_comment'),
+    path('admin/review-comments/', AdminCommentReviewView.as_view(), name='admin_review_comments_api'),
+    path('blogs/comments/', CommentAPIView.as_view(), name='comment'),
+
     
 ]
