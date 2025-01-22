cbd24a934fdfa41ad77f18ac8ea829dba553e67d
Aayushman Amatya <120630653+Aayushman4725@users.noreply.github.com>
2025-01-22 08:30:03 +0545

    Delete backend/authenticate directory

--- backend/authenticate/admin.py
+++
@@ -1,8 +0,0 @@
-
-
-# Register your models here.
-from django.contrib import admin
-from .models import CustomUser,Profile
-# Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Profile)
