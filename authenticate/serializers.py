aea27dec860c7782a7803153316cebe178de61b4
Aashish <aashish.211607@ncit.edu.np>
2025-02-21 08:28:17 +0545

    updates

--- authenticate/serializers.py
+++ authenticate/serializers.py
@@ -4,3 +4,11 @@ from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.password_validation import validate_password
 from .tokens import generate_token
 from .models import Profile
+
+User = get_user_model()
+
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,min_length=8)
+    confirm_password = serializers.CharField(write_only=True, min_lenght=8)
+
+    
