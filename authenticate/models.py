798e48a3bb8c7d70b9a15553ff80d675a00c0410
Aashish <aashish.211607@ncit.edu.np>
2025-02-21 10:04:50 +0545

    update

--- authenticate/models.py
+++ authenticate/models.py
@@ -3,13 +3,11 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
 from django.conf import settings
-from blog.models import Blog   
-
-
+from blog.models import Blog
+# Create your models here.
 class CustomUser(AbstractUser):
     username = models.CharField(_("Username"), max_length=100, unique=True)
     email = models.EmailField(_("Email"), unique=True)
-    
     # comment = models.TextField(_("Comment"), blank=True, null=True)
 
     USERNAME_FIELD = 'email'
@@ -17,30 +15,12 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        related_name='custom_user_set',
-        blank=True,
-        verbose_name=_('groups'),
-        help_text=_(
-            'The groups this user belongs to. A user will get all permissions '
-            'granted to each of their groups.'
-        ),
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        related_name='custom_user_set',
-        blank=True,
-        verbose_name=_('user permissions'),
-        help_text=_('Specific permissions for this user.'),
-    )
-
     def __str__(self):
         return self.username
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
-    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
     comment_text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_negative = models.BooleanField(default=False)
@@ -48,12 +28,24 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.blog.title}"
+
     
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     profile_picture = models.ImageField(default='images/profile_picture/default.jpg', upload_to='images/profile_picture')
-    phone_number = models.CharField(_("Phone Number"), max_length=15, blank=True, null=True)
+    phone_number = models.CharField(("Phone Number"), max_length=15, blank=True, null=True)
     about = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.user.username
+    
+
+
+
+
+
+
+
+
+
+   
