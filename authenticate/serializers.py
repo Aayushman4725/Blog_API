8df025555458a1fc7af4cf83710b82efe144057b
Aashish <aashish.211607@ncit.edu.np>
2025-02-21 09:30:58 +0545

    update

--- authenticate/serializers.py
+++ authenticate/serializers.py
@@ -36,5 +36,12 @@ class LoginSerializer (serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only = True)
 
-    def valida
+    def validate(self,data):
+        user = authenticate(email=data['email'], password=data['password'])
+        if not user:
+            raise serializers.ValidationError({"detail": "Invalid credentials."})
+        if not user.is_active:
+            raise serializers.ValidationError({"detail": "Account is not activated."})
+        return user
+
 
